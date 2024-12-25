d40c0968bd10d2386bacfff96a084fe283e7994d
trangiabao2702 <meikaruku@gmail.com>
2024-12-25 23:30:05 +0700

    apply animations idle and walk for player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5f;
+    private bool isWalking = false;
 
     // Start is called before the first frame update
     void Start()
@@ -39,5 +40,12 @@ public class Player : MonoBehaviour
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
         transform.position += moveDir * moveSpeed * Time.deltaTime;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, moveSpeed * Time.deltaTime);
+
+        isWalking = moveDir != Vector3.zero;
+    }
+
+    public bool IsWalking()
+    {
+        return isWalking;
     }
 }
