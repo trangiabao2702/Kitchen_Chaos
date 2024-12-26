758dfcdd3adad7e8440b757d1b5c60c787bf4376
trangiabao2702 <meikaruku@gmail.com>
2024-12-27 00:18:37 +0700

    apply pickup/drop item from/to counter

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -8,6 +8,31 @@ public class ClearCounter : BaseCounter
 
     public override void Interact(Player player)
     {
-        
+        if (!HasKitchenObject())
+        {
+            // Counter is empty
+            if (player.HasKitchenObject())
+            {
+                // Player is carrying item
+                player.GetKitchenObject().SetKitchenObjectParent(this);
+            }
+            else
+            {
+                // Player is not carrying anything
+            }
+        }
+        else
+        {
+            // Counter is not empty
+            if (player.HasKitchenObject())
+            {
+                // Player is carrying item
+            }
+            else
+            {
+                // Player is not carrying anything
+                this.GetKitchenObject().SetKitchenObjectParent(player);
+            }
+        }
     }
 }
