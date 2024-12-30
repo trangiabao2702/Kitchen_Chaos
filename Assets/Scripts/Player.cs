ec877a46ede10e74d9a228925c8ccbfcf75a5359
trangiabao2702 <meikaruku@gmail.com>
2024-12-30 18:03:00 +0700

    handle game start

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -43,6 +43,8 @@ public class Player : MonoBehaviour, IKitchenObjectParent
 
     private void GameInput_OnInteractAction(object sender, System.EventArgs e)
     {
+        if (!KitchenGameManager.Instance.IsGamePlaying()) return;
+
         if (selectedCounter !=  null)
         {
             selectedCounter.Interact(this);
@@ -51,6 +53,8 @@ public class Player : MonoBehaviour, IKitchenObjectParent
 
     private void GameInput_OnInteractAlternateAction(object sender, EventArgs e)
     {
+        if (!KitchenGameManager.Instance.IsGamePlaying()) return;
+
         if (selectedCounter != null)
         {
             selectedCounter.InteractAlternate(this);
